edb99bb7415f0836c47f5326e782b3544128e2ae
Lev# <mr.lmartirosyan888@gmail.com>
2019-01-28 20:15:44 +0400

    the first

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your first numbre:");
+            int tiv1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write your sign: (+,-,*,/,%)");
+            string sign = Console.ReadLine();
+            if (tiv1 != 0)
+                Console.WriteLine("Write your second numbre:");
+            int tiv2 = Convert.ToInt32(Console.ReadLine());
+
+           
+
+            switch (sign)
+            {
+                case "+":
+                    Console.WriteLine("{0} + {1} = {2}", tiv1, tiv2, tiv1 + tiv2);
+                    break;
+                case "-":
+                    Console.WriteLine("{0} - {1} = {2}", tiv1, tiv2, tiv1 - tiv2);
+                    break;
+                case "*":
+                    Console.WriteLine("{0} * {1} = {2}", tiv1, tiv2, tiv1 * tiv2);
+                    break;
+                case "%":
+                    Console.WriteLine("{0} % {1} = {2}", tiv1, tiv2, tiv1 % tiv2);
+                    break;
+                case "/":
+                    if (tiv2 != 0)
+                        Console.WriteLine("{0} / {1} = {2}", tiv1, tiv2, (float)tiv1 / tiv2);
+                    else
+                        Console.WriteLine("Զրոի! ");
+                    break;
+
+                default:
+                    Console.WriteLine("Սխալ նշան է մուտք արվել");
+                    break;
+
+            }
+            Console.ReadKey();
+        
+        }
+    }
+}
